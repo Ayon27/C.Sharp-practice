10c5ce04980787abbb5b7675ef0395ea9e84a548
ayon27 <ayon8295@gmail.com>
2020-05-13 23:21:14 +0600

    initial commit. wk2 done

---
+++ Week 1 Assignment/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome.. blah blah blah!");
+
+            Console.Write("Enter first x value: ");
+            float x1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter first y value: ");
+            float y1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter second x value: ");
+            float x2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter second y value: ");
+            float y2 = float.Parse(Console.ReadLine());
+
+            Test t = new Test();
+
+            Console.WriteLine("Distance between points: " + Math.Round(t.dist(x1, y1, x2, y2), 3));
+            Console.WriteLine("Angle between points: " + Math.Round(t.angle(x1, y1, x2, y2), 3) + " degrees");
+        }
+
+        float dist(float x1, float y1, float x2, float y2)
+        {
+            return (float) Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        float angle(float x1, float y1, float x2, float y2)
+        {
+            return (float) (Math.Atan2(x2 - x1, y2 - y1) * 180 / Math.PI);
+        }
+    }
+}
---
+++ Week 2 Assignment/ProgrammingAssignment2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// Programming Assignment 2 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Deals 2 cards to 3 players and displays cards for players
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // print welcome message
+            Console.WriteLine("Welcome \n");
+            // create and shuffle a deck
+            Deck deck = new Deck();
+            deck.Shuffle();
+            // deal 2 cards each to 3 players (deal properly, dealing
+            // the first card to each player before dealing the
+            // second card to each player
+
+            Card[,] player = new Card[3, 2]; // each row represents a player's cards
+
+            for (int col = 0; col < player.GetLength(1); col++) // the array fills column wise to ensure proper dealing
+            {
+                for (int row = 0; row < player.GetLength(0); row++)
+                {
+                    player[row, col] = deck.TakeTopCard();
+                }
+            }
+            
+            // flip all the cards over
+            for (int row = 0; row < player.GetLength(0); row++) // cards are flipped row wise
+            {
+                for (int col = 0; col < player.GetLength(1); col++)
+                {
+                    player[row, col].FlipOver();
+                }
+            }
+
+            // print the cards for player 1
+
+            for (int row = 0; row < player.GetLength(0); row++) // all player's cards are printed
+            {
+                Console.WriteLine("Player " + (row + 1) + ": ");
+                
+                for (int col = 0; col < player.GetLength(1); col++)
+                {
+                    Console.WriteLine(player[row, col].Rank + " of " + player[row, col].Suit);
+                }
+                Console.WriteLine();
+            }
+
+            // // print the cards for player 2
+
+            // // print the cards for player 3
+
+        }
+    }
+}
